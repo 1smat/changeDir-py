060812d39acbfcf71a4ad25a43fb82d2df00ac7e
ismatullo <57567832+1smat@users.noreply.github.com>
2021-08-12 10:22:33 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import datetime, os, time
 import schedule #  $ pip install schedule 
 
+currentDate = datetime.datetime.today().strftime("%d.%m.%Y")  # get current date
+todayDate = currentDate
+
+print('todayDate ' + todayDate)
+print('currentDate: ' + currentDate)
+
 def sorter():
-    print('ever day')
+    global todayDate, currentDate
 
-    td = datetime.datetime.today()
-    currentDate = td.strftime("%d.%m.%Y") # get current date 
-    todayDate = currentDate  # set current date to today
-
-    print(currentDate)
-
-    if todayDate != currentDate:
-        currentDate = td.strftime("%d.%m.%Y")
-        print('if ishladi')
+    if todayDate != datetime.datetime.today().strftime("%d.%m.%Y"):
+        currentDate = datetime.datetime.today().strftime("%d.%m.%Y")
+        print('Changing dir names...')
         os.rename('Kunlik', todayDate)
         os.mkdir('Kunlik')
         todayDate = currentDate
-    	# changing folder's name  
+        print('todayDate: ' + todayDate)
+        print('currentDate: ' + currentDate)
+    	# changing folder's name 
 
-sorter()     
-schedule.every().day.at("01:00").do(sorter)
-
+schedule.every().day.at("00:00").do(sorter)
 
 while True:
     schedule.run_pending()
